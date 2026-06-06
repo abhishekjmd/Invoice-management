3c58cc16d0e23d3f6c049b3d73932d0611f98711
abhishekjmd <abhishektiwariisro1278@gmail.com>
2026-06-06 06:22:32 +0530

    create database connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,11 @@
+require("dotenv").config();
 const app = require("./app");
-
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  console.log("connected to database");
+  app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+  });
 });
